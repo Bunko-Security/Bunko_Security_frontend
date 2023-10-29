8eed5bacca26211ccb4ac3859f4b5579ca737fd6
Shiza <vnchernovv02@gmail.com>
2023-10-30 00:06:48 +0300

    add label & remove overlay

--- src/components/forms/InputSearch/InputSearch.tsx
+++ src/components/forms/InputSearch/InputSearch.tsx
@@ -3,8 +3,7 @@
 import styles from "./InputSearch.module.scss";
 import IconSearch from "/public/icon-search.svg";
 import { useForm } from "react-hook-form";
-import { motion, AnimatePresence } from "framer-motion";
-import { ChangeEventHandler, FC, InputHTMLAttributes, useRef, useState } from "react";
+import { ChangeEventHandler, FC, InputHTMLAttributes, useRef } from "react";
 
 type FormValues = {
 	file_name: string;
@@ -12,83 +11,51 @@ type FormValues = {
 
 interface InputSearchProps extends InputHTMLAttributes<HTMLInputElement> {
 	marginBottom?: string | number;
+	textLabel?: string;
 }
 
-const InputSearch: FC<InputSearchProps> = ({ marginBottom, ...props }) => {
+const InputSearch: FC<InputSearchProps> = ({ marginBottom, textLabel, ...props }) => {
 	const formRef = useRef<HTMLFormElement>(null);
-	const [showSearch, setShowSearch] = useState<boolean>(false);
 	const { register, handleSubmit, reset } = useForm<FormValues>();
 
 	const onChange: ChangeEventHandler<HTMLInputElement> = ({ target: { value } }) => {
 		if (value.length) {
-			formRef.current!.style.boxShadow = "0 0 5px 1px var(--violet-dark)";
-			setShowSearch(true);
-		} else {
-			formRef.current!.style.boxShadow = "none";
-			setShowSearch(false);
 		}
 	};
 
-	const handleClickInput = () => {
-		formRef.current!.style.boxShadow = "0 0 5px 1px var(--violet-dark)";
-		setShowSearch(true);
-	};
-
-	const handleClickOverlay = () => {
-		formRef.current!.style.boxShadow = "none";
-		setShowSearch(false);
-	};
-
 	const onSubmit = (values: FormValues) => {
-		formRef.current!.style.boxShadow = "none";
 		console.log(values);
-		setShowSearch(false);
 		reset();
 	};
 
 	return (
 		<>
-			<AnimatePresence>
-				{showSearch && (
-					<motion.div
-						className={styles.overlay}
-						onClick={handleClickOverlay}
-						initial={{ opacity: 0 }}
-						animate={{ opacity: 1 }}
-						exit={{ opacity: 0 }}
-						transition={{ duration: 0.15, ease: "linear" }}
-					/>
-				)}
-			</AnimatePresence>
-
 			<form
 				ref={formRef}
 				className={styles.form_search}
 				style={{ marginBottom }}
 				onSubmit={handleSubmit(onSubmit)}
 			>
-				<input
-					{...register("file_name", {
-						onChange: onChange,
-					})}
-					className={styles.input}
-					type="search"
-					autoComplete="off"
-					onClick={handleClickInput}
-					{...props}
-				/>
-				<button
-					type="submit"
-					className={styles.button}
-				>
-					<IconSearch className={styles.icon_search} />
-				</button>
-
-				{showSearch && (
-					<div className={styles.search_result}>
-						<div>Efwefwef</div>
+				<label className={styles.label}>
+					{textLabel && <span>{textLabel}</span>}
+					<div className={styles.search}>
+						<input
+							{...register("file_name", {
+								onChange: onChange,
+							})}
+							className={styles.input}
+							type="search"
+							autoComplete="off"
+							{...props}
+						/>
+						<button
+							type="submit"
+							className={styles.button}
+						>
+							<IconSearch className={styles.icon_search} />
+						</button>
 					</div>
-				)}
+				</label>
 			</form>
 		</>
 	);
