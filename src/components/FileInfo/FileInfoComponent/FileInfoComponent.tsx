dc9a50892daf06c594f3485b686b38dc46278a59
Shiza <vnchernovv02@gmail.com>
2023-10-30 00:14:28 +0300

    add comp ModalForwardFile & add comp Checkbox

--- src/components/FileInfo/FileInfoComponent/FileInfoComponent.tsx
+++ src/components/FileInfo/FileInfoComponent/FileInfoComponent.tsx
@@ -3,20 +3,29 @@
 import styles from "./FileInfoComponent.module.scss";
 import IconForward from "/public/icon-forward.svg";
 import IconDownload from "/public/icon-download.svg";
-import { FC } from "react";
+import ModalForwardFile from "@/components/modals/ModalForwardFile/ModalForwardFile";
+import { CSSProperties, FC, useState } from "react";
 
 interface FileInfoComponentProps {
 	fileName: string;
 	fileAdd: string;
 	fileDate: string;
+	style?: CSSProperties;
 }
 
-const FileInfoComponent: FC<FileInfoComponentProps> = ({ fileAdd, fileName, fileDate }) => {
-	const handleForward = () => {};
+const FileInfoComponent: FC<FileInfoComponentProps> = ({ fileAdd, fileName, fileDate, style }) => {
+	const [openForward, setOpenForward] = useState<boolean>(false);
+
+	const handleForward = () => {
+		setOpenForward(true);
+	};
 	const handleDownload = () => {};
 
 	return (
-		<div className={styles.file}>
+		<div
+			className={styles.file}
+			style={style}
+		>
 			<p className={styles.file_name}>{fileName}</p>
 			<p className={styles.file_add}>{fileAdd}</p>
 			<p className={styles.file_date}>{fileDate}</p>
@@ -30,6 +39,8 @@ const FileInfoComponent: FC<FileInfoComponentProps> = ({ fileAdd, fileName, file
 					onClick={handleDownload}
 				/>
 			</div>
+
+      <ModalForwardFile isOpen={openForward} onClose={() => setOpenForward(false)}/>
 		</div>
 	);
 };
---
+++ src/components/forms/formItems/Checkbox.tsx
@@ -0,0 +1,21 @@
+import styles from "./Checkbox.module.scss";
+import { FC, InputHTMLAttributes } from "react";
+
+const Checkbox: FC<InputHTMLAttributes<HTMLInputElement>> = ({ style, className, ...props }) => {
+	return (
+		<label
+			className={`${styles.label} ${className}`}
+			style={{ ...style, ...(props.disabled && { cursor: "not-allowed" }) }}
+		>
+			<input
+				className={styles.checkbox}
+				type="checkbox"
+				{...props}
+			/>
+			<span className={styles.custom_checkbox} />
+			<span className={styles.checkbox_text}>Петров И.В.</span>
+		</label>
+	);
+};
+
+export default Checkbox;
