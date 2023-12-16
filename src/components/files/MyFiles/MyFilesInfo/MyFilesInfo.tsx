f8f8333631d55203eef18344ce32246844f24cc2
Shiza <vnchernovv02@gmail.com>
2023-12-16 22:55:14 +0300

    add btn delete for file

--- src/components/files/MyFiles/MyFilesInfo/MyFilesInfo.tsx
+++ src/components/files/MyFiles/MyFilesInfo/MyFilesInfo.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import styles from "./MyFilesInfo.module.scss";
+import IconDelete from "/public/icon-close.svg";
 import IconDownload from "/public/icon-download.svg";
 import type { IFile } from "@/models/file.model";
 import type { CSSProperties, FC } from "react";
@@ -19,7 +20,16 @@ const MyFilesInfo: FC<MyFilesInfoProps> = ({ file, style }) => {
 		>
 			<p className={styles.file_name}>{file.filename}</p>
 			<p className={styles.file_date}>{file.delete_date}</p>
-			<IconDownload className={styles.icon_download} />
+			<div className={styles.buttons}>
+				<IconDelete
+					className={styles.icon_delete}
+					data-action="delete"
+				/>
+				<IconDownload
+					className={styles.icon_download}
+					data-action="download"
+				/>
+			</div>
 		</div>
 	);
 };
