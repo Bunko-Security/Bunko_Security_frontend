c81b79c940cae331d2ad28a8765fba07cecfc653
Shiza <vnchernovv02@gmail.com>
2023-12-16 22:58:26 +0300

    update friends page

--- src/app/(home)/profile/friends/page.tsx
+++ src/app/(home)/profile/friends/page.tsx
@@ -1,3 +1,6 @@
+import styles from "./page.module.scss";
+import EmployeesList from "@/components/friends/EmployeesList/EmployeesList";
+import FriendsList from "@/components/friends/FriendsList/FriendsList";
 import type { Metadata, NextPage } from "next";
 
 export const metadata: Metadata = {
@@ -5,7 +8,21 @@ export const metadata: Metadata = {
 };
 
 const Friends: NextPage = () => {
-	return <div>Friends</div>;
+	return (
+		<div className={styles.friends}>
+			<h1 className={styles.title}>Мои друзья</h1>
+			<div className={styles.wrapper}>
+				<div className={styles.wrapper_people}>
+					<h2>Добавить в друзья</h2>
+					<EmployeesList />
+				</div>
+				<div className={styles.wrapper_friends}>
+					<h2>В друзьях</h2>
+					<FriendsList />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Friends;
